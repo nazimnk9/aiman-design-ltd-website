8614861acb3b80dabe367c9e9b6900131d4c3b5b
Md. Nazim Ahmed <77225770+nazimnk9@users.noreply.github.com>
2025-12-03 18:45:03 +0600

    solve

--- components/product-categories-sidebar.tsx
+++ components/product-categories-sidebar.tsx
@@ -1483,13 +1483,13 @@ export function ProductCategoriesSidebar({
                       : "border-gray-200 hover:border-gray-900"
                       }`}
                   >
-                    <div className="w-50 h-50 flex items-center justify-center">
+                    <div className="w-32 h-50 flex items-center justify-center">
                       <Image
                         src={category.icon || "/placeholder.svg"}
                         alt={category.label}
                         width={350}
                         height={550}
-                        className="w-full h-full object-contain"
+                        className="w-full h-full object-cover"
                       />
                     </div>
                   </button>
